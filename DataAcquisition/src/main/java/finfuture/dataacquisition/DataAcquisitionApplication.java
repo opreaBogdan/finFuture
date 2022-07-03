1bff5ea64c9c646b8aae55cb6b521c976b265f81
opreaBogdan <oprea.bg@gmail.com>
2022-07-03 18:18:58 +0300

    Configured ConfigServer & Eureka Server

--- DataAcquisition/src/main/java/finfuture/dataacquisition/DataAcquisitionApplication.java
+++ DataAcquisition/src/main/java/finfuture/dataacquisition/DataAcquisitionApplication.java
@@ -1,39 +1,67 @@
 package finfuture.dataacquisition;
 
+import com.netflix.discovery.EurekaClient;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@EnableScheduling
+//@EnableScheduling
 @SpringBootApplication
+@RestController
 public class DataAcquisitionApplication {
+
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
+
+    @Value("${spring.application.name}")
+    private String appName;
 
     static final String topicExchangeName = "spring-boot-exchange";
 
     static final String queueName = "spring-boot";
 
-    @Bean
-    Queue queue() {
-        return new Queue(queueName, false);
+    @Value("${user.role}")
+    private String role;
+
+    @GetMapping(
+            value = "/whoami/{username}",
+            produces = MediaType.TEXT_PLAIN_VALUE)
+    public String whoami(@PathVariable("username") String username) {
+        return String.format("Hello! You're %s and you'll become a(n) %s...\n", username, role);
     }
 
-    @Bean
-    TopicExchange exchange() {
-        return new TopicExchange(topicExchangeName);
-    }
-
-    @Bean
-    Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
-    }
+//    @Bean
+//    Queue queue() {
+//        return new Queue(queueName, false);
+//    }
+//
+//    @Bean
+//    TopicExchange exchange() {
+//        return new TopicExchange(topicExchangeName);
+//    }
+//
+//    @Bean
+//    Binding binding(Queue queue, TopicExchange exchange) {
+//        return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+//    }
 
 //    @Bean
 //    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
